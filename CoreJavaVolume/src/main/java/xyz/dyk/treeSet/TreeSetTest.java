90347af14dde81570c7a214cdfa57d12a9e5ec4f
daiyingkai <1345133130@qq.com>
2020-11-18 00:22:06 +0800

    2020年11月18日 00:22:02

---
+++ CoreJavaVolume/src/main/java/xyz/dyk/treeSet/TreeSetTest.java
@@ -0,0 +1,24 @@
+package xyz.dyk.treeSet;
+
+import java.util.Comparator;
+import java.util.NavigableSet;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class TreeSetTest {
+    public static void main(String[] args) {
+        SortedSet<Item> parts = new TreeSet<>();
+        parts.add(new Item("Toaster", 1234));
+        parts.add(new Item("Widget", 4562));
+        parts.add(new Item("Modem", 9912));
+        System.out.println(parts);
+
+        NavigableSet<Item> items = new TreeSet<>(
+                Comparator.comparing(Item::getDescription)
+        );
+
+        items.addAll(parts);
+
+        System.out.println(items);
+    }
+}
