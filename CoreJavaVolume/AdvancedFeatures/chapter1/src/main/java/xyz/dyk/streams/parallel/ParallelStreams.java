b4daa758dfdd230c4a660b4b51809dbbef4b2879
daiyingkai <1345133130@qq.com>
2020-12-05 01:00:40 +0800

    2020年12月5日 01:00:35

---
+++ CoreJavaVolume/AdvancedFeatures/chapter1/src/main/java/xyz/dyk/streams/parallel/ParallelStreams.java
@@ -0,0 +1,58 @@
+package xyz.dyk.streams.parallel;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
+
+import static java.util.stream.Collectors.*;
+
+public class ParallelStreams {
+    public static void main(String[] args) throws IOException {
+
+        Path path = Paths.get("C:/Users/k1345/Desktop/redis.profile");
+
+        String context = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+        List<String> wordList = Arrays.asList(context.split("\\PL+"));
+
+        //  Very bad code ahead
+        int[] shortWords = new int[10];
+        wordList.parallelStream().forEach(
+                s -> {
+                    if (s.length() < 10) {
+                        shortWords[s.length()]++;
+                    }
+                });
+        System.out.println(Arrays.toString(shortWords));
+
+        //  Try again-the result will likely be different (and also wrong)
+        Arrays.fill(shortWords, 0);
+        wordList.parallelStream().forEach(
+                s -> {
+                    if (s.length() < 10) {
+                        shortWords[s.length()]++;
+                    }
+                });
+        System.out.println(Arrays.toString(shortWords));
+
+        //  Remedy: Group and count
+        Map<Integer, Long> shortWordCounts = wordList.parallelStream().filter(s -> s.length() < 10).collect(groupingBy(String::length, counting()));
+        System.out.println(shortWordCounts);
+
+        //  Downstream order not deterministic
+        ConcurrentMap<Integer, List<String>> result = wordList.parallelStream().collect(groupingByConcurrent(String::length));
+        System.out.println(result.get(14));
+
+        result = wordList.parallelStream().collect(groupingByConcurrent(String::length));
+        System.out.println(result.get(14));
+
+        ConcurrentMap<Integer, Long> wordCounts = wordList.parallelStream().collect(groupingByConcurrent(String::length, counting()));
+        System.out.println("wordCounts = " + wordCounts);
+
+    }
+}
